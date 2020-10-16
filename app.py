7b75257a02d1abcde77a8faa04ee22646d4b78e0
veryprofessionalusername <amr21240@gmai.com>
2020-10-17 03:11:16 +0800

    currently displays a place holder image in the sentry setup webpage

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
+import os
+import cv2
 
 app = Flask(__name__)
+
+placeholder_img = os.path.join('static', 'images/placeholder.jpg')
+
 
 @app.route('/')
 def hello_world():
@@ -8,7 +13,16 @@ def hello_world():
 
 @app.route('/setup_sentry')
 def setup_sentry():
-	return 'Hello, World!'
+	webcam = cv2.VideoCapture(0)
+	# webcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+	# webcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+
+	if webcam.isOpened():
+		_, frame = webcam.read()
+		_, frame = cv2.imencode('.jpg', frame).tobytes()
+		return render_template('sentry_setup_template.html', placeholder_img=frame)
+	else:
+		return render_template('sentry_setup_template.html', placeholder_img=placeholder_img)
 	
 
 if __name__ == '__main__':
